3b36d8dbd4e51bcf868fb1d12fec0625c9c974f5
waelxl <112487917+waelxl@users.noreply.github.com>
2022-12-12 00:56:00 +0100

    new

--- src/main/kotlin/lms_133/Task9.kt
+++ src/main/kotlin/lms_133/Task9.kt
@@ -1,15 +1,23 @@
 package lms_133
 
 fun harshad(number: Int): Boolean{
+    if (number == 0) return false
 
-    val numberAsString = number.toString()
-    var total = 0
-
-    for(digit in numberAsString){
-        total += digit.digitToInt()
+    var digitSum = 0
+    number.toString().forEach { digit ->
+        digitSum += digit.digitToInt()
     }
-    return number % total == 0
+    return number % digitSum == 0
 }
-fun main(){
-    println(harshad(number = 11))
+fun main() {
+
+    println(harshad(number = 1)) //
+    println(harshad(number = 2)) //
+    println(harshad(number = 10)) //
+    println(harshad(number = 12)) //
+
+    println(harshad(number = 0)) //
+    println(harshad(number = 11)) //
+    println(harshad(number = 13)) //
+    println(harshad(number = 15)) //
 }
