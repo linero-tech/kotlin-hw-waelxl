86814ee6d71e28f80e760677b363b19a1bf08ae3
waelxl <112487917+waelxl@users.noreply.github.com>
2022-12-12 01:07:26 +0100

    task task

--- src/main/kotlin/lms_133/Task5.kt
+++ src/main/kotlin/lms_133/Task5.kt
@@ -1,20 +1,10 @@
 package lms_133
 
-fun groceries(groceryList: List<Map<String, Any>>): Double {
+fun groceries(list: List<Map<String, Any>>): Double {
     var result = 0.0
-    if(groceryList.isEmpty()) {
-        result
-    }else{
-        groceryList.forEach  { input ->
-            val input1 = input.getValue("quantity")
-            val input2 = input.getValue("price")
-            result = input1 as Int * input2 as Double
-        }
+    list.forEach { map ->
+        result += map.getValue("quantity").toString().toInt() * map.getValue("price").toString().toDouble()
     }
-    return result
-}
 
-fun main(){
-    println(groceries(groceryList =
-    listOf(mapOf("product" to "milk", "quantity" to 1, "price" to 1.50))))
+    return result
 }
--- src/main/kotlin/lms_133/Task6.kt
+++ src/main/kotlin/lms_133/Task6.kt
@@ -1,18 +1,21 @@
 package lms_133
 
 fun counter(word: String): Boolean {
-    val alphabet = listOf('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z')
+    val alphabet = ('a'..'z')
     var indexSum = 0
-    word.lowercase().forEach {letter ->
-        if (letter in alphabet) {
-            val index = alphabet.indexOf(letter)+1
+
+    word.lowercase().forEach { letter ->
+        if (letter in alphabet){
+            val index = alphabet.indexOf(letter) + 1
             indexSum += index
         }
-
     }
     return indexSum % 2 == 0
 }
 
 fun main(){
-    println(counter("a a"))
+    println(counter("alexa"))
+    println(counter("aa"))
+    println(counter("ab"))
+    println(counter("aa aa"))
 }
